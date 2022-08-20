2dc98ee14be4c87d52c39983c1d9e0f122c8bc9e
coderdrew26 <josephandrewnepomuceno@gmail.com>
2022-08-20 17:57:46 +0800

    init

---
+++ ExampleRandom.java
@@ -0,0 +1,8 @@
+import java.util.random.*;
+
+public class ExampleRandom {
+    public static void main(String[] args) {
+        int x = (int) (Math.random() * 6) + 3;
+        System.out.println(x);
+    }
+}
